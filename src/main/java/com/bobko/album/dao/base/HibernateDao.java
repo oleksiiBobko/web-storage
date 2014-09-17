b6bd3f47468f18450508897a803bf646e736a651
oleksii <oleksii@oleksii.debian.oleksii.com>
2014-09-18 00:09:44 +0300

    formatting

--- src/main/java/com/bobko/album/dao/base/HibernateDao.java
+++ src/main/java/com/bobko/album/dao/base/HibernateDao.java
@@ -16,63 +16,66 @@ import org.springframework.transaction.annotation.Propagation;
 
 /**
  * Basic DAO operations dependent with Hibernate's specific classes
+ * 
  * @see SessionFactory
  */
-@Transactional(propagation = Propagation.REQUIRED, readOnly=false)
-public class HibernateDao<E, K extends Serializable> implements GenericDao<E, K> {
+@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+public class HibernateDao<E, K extends Serializable> implements
+		GenericDao<E, K> {
 
-    @Autowired
+	@Autowired
 	private SessionFactory sessionFactory;
-    
-    protected Class<? extends E> daoType;
 
-    @SuppressWarnings("unchecked")
+	protected Class<? extends E> daoType;
+
+	@SuppressWarnings("unchecked")
 	public HibernateDao() {
-        daoType = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-    }
+		daoType = (Class<E>) ((ParameterizedType) getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0];
+	}
 
-    @Autowired
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-    protected Session currentSession() {
-        return sessionFactory.getCurrentSession();
-    }
+	protected Session currentSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
-    @Override
-    public void add(E entity) {
-        currentSession().save(entity);
-    }
-
-    @Override
-    public void update(E entity) {
-        currentSession().saveOrUpdate(entity);
-    }
-
-    @Override
-    public void remove(E entity) {
-        currentSession().delete(entity);
-    }
-
-    @SuppressWarnings("unchecked")
 	@Override
-    public E find(K key) {
-        return (E) currentSession().get(daoType, key);
-    }
+	public void add(E entity) {
+		currentSession().save(entity);
+	}
 
-    @SuppressWarnings("unchecked")
 	@Override
-    public List<E> list() {
-        return currentSession().createCriteria(daoType).list();
-    }
+	public void update(E entity) {
+		currentSession().saveOrUpdate(entity);
+	}
+
+	@Override
+	public void remove(E entity) {
+		currentSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public E find(K key) {
+		return (E) currentSession().get(daoType, key);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<E> list() {
+		return currentSession().createCriteria(daoType).list();
+	}
 
 	@SuppressWarnings("unchecked")
 	public List<E> rankList(int shift, int count) {
-        Criteria criteria = currentSession().createCriteria(daoType);
-        criteria.setFirstResult(shift * count);
-        criteria.setMaxResults(count);
-        criteria.addOrder(Order.desc("created"));
-        return criteria.list();
+		Criteria criteria = currentSession().createCriteria(daoType);
+		criteria.setFirstResult(shift * count);
+		criteria.setMaxResults(count);
+		criteria.addOrder(Order.desc("created"));
+		return criteria.list();
 	}
 }

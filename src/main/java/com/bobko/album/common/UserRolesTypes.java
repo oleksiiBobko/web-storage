bf2c0075409b095abee74e63ffe9f446b32bd860
oleksii <skellag@gmail.com>
2014-12-15 18:29:08 +0200

    add anonimouce user role

--- src/main/java/com/bobko/album/common/UserRolesTypes.java
+++ src/main/java/com/bobko/album/common/UserRolesTypes.java
@@ -6,6 +6,7 @@ package com.bobko.album.common;
 public class UserRolesTypes {
 
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_ANONYMOUS = "ROLE_ADMIN";
 
     private UserRolesTypes() {
     }

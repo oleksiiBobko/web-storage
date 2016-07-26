b4e5c4d5226036da6969d19fcd83ca65aa63214c
oleksiiBobko <skellag@gmail.com>
2016-07-26 16:21:46 +0300

    commit 3

--- src/main/java/com/bobko/album/common/Main.java
+++ src/main/java/com/bobko/album/common/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String regex = "^(?!\\.)([a-zA-Z0-9-_\\.!?*+]*)@([a-zA-Z0-9-_\\.!?*+]*)$";
-        String regex1 = "rx1";
+        String regex133 = "rx1";
         boolean b = Pattern.matches(regex, "ole..ksii.bobko@somemail.org.kz");
         System.err.println(b);
     }
